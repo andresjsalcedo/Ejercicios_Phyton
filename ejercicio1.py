cb7329fc09f2ccdb7e0cbbf2c9104ca9e8adab44
andresjsalcedo <andresj_salcedo@soy.sena.edu.co>
2024-07-17 22:04:27 -0500

    ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,5 @@
+#Ejercisio 1
+print("La felicidad se puede encontrar hasta en los mas oscuros momentos, si somos capaces de usar bien la luz - Albus Dumbledore")
+#Print cumple la funcion de mostrar o ejecutar en consola
+
+#Ejercicio 2
